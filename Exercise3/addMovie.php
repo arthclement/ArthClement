5e344acdc3d885e4d8ed9eab72446db3071aa116
arthclement <arthclems@gmail.com>
2018-03-14 14:33:45 +0100

    Created HTML Form

---
+++ Exercise3/addMovie.php
@@ -0,0 +1,11 @@
+<?php
+
+if (
+    strlen($_POST['title']) > 5
+    && strlen($_POST['director']) > 5
+    && strlen($_POST['actors']) > 5
+    && strlen($_POST['producer']) > 5
+    && strlen($_POST['synopsis']) > 5    
+) {
+
+}
